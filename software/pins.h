cebf8f959490cec37bdb51785bd480d01c511271
Andrew Cassidy <drewcassidy@me.com>
2016-12-17 14:44:31 -0800

    Correct error in pins.h

--- software/pins.h
+++ software/pins.h
@@ -3,10 +3,10 @@
 
 #define	CHG 		(p1)
 #define IND			(p2)
-#define LCD_RST		(p2)
-#define LCD_CE		(p3)
-#define LCD_DC		(p4)
-#define LCD_MOSI 	(p5)
+#define LCD_RST		(p3)
+#define LCD_CE		(p4)
+#define LCD_DC		(p5)
+#define LCD_MOSI 	(p6)
 #define LCD_CLK		(p7)
 
 #endif
