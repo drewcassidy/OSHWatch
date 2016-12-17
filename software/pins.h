dfba34c141c98c2dec7262c4b05ef2a1329b042a
Andrew Cassidy <drewcassidy@me.com>
2016-12-17 14:58:52 -0800

    expand pins.h to include buttons, I2C, and misc outputs

--- software/pins.h
+++ software/pins.h
@@ -8,5 +8,17 @@
 #define LCD_DC		(p5)
 #define LCD_MOSI 	(p6)
 #define LCD_CLK		(p7)
+#define BACKLIGHT   (p12)
+#define ACC_INT1    (p13)
+#define ACC_INT2    (p14)
+#define SDA         (p15)
+#define SDL         (p16)
+#define BTN_BACK    (p17)
+#define VIB         (p21)
+#define BTN_SELECT  (p25)
+#define BTN_DOWN    (p26)
+#define BTN_UP      (p27)
+#define BTN_RIGHT   (p28)
+#define BTN_LEFT    (p29)
 
 #endif
